1da064e6dc13a23433f6b18e9b8dab510d154012
giovanni <giovanni_ge@hotmai.com>
2025-02-04 21:18:53 -0300

    feat: adicionando arquivos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import "./main.css"
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+`
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
 )
