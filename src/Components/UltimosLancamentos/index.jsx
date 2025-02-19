628a060f9555b427a39a18ea780223642f1e9a91
giovanni <giovanni_ge@hotmai.com>
2025-02-18 22:10:29 -0300

    feat: adicionando codigo novo

--- src/Components/UltimosLancamentos/index.jsx
+++ src/Components/UltimosLancamentos/index.jsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 import { livros } from './DadosUltimosLancamentos';
+import { Titulo } from '../Titulo';
+import CardRecomenda from '../CardRecomenda';
+import imagemLivro from "../../imagens/livro2.png";
+
 
 const UltimosLancamentoContainer = styled.section`
       background-color: #EBECEE;
@@ -8,15 +12,7 @@ const UltimosLancamentoContainer = styled.section`
     flex-direction: column;
 `;
 
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
-`;
+
 
 const NovosLivrosContainer = styled.div`
     margin-top: 30px;
@@ -24,18 +20,34 @@ const NovosLivrosContainer = styled.div`
     width: 100%;
     justify-content: center;
     cursor: pointer;
-`
+`;
 
 export default function UltimosLancamentos() {
   return (
     <UltimosLancamentoContainer>
-      <Titulo>ultimos lancamentos</Titulo>
+      <Titulo cor="#EB9B00"
+        tamanhoFonte="36px">
+        Últimos lançamentos
+      </Titulo>
       <NovosLivrosContainer>
         {livros.map(livro => (
           <img src={livro.src} key={livro.id} />
         ))}
       </NovosLivrosContainer>
-   
+
+      <CardRecomenda 
+        titulo="Talvez você se interesse por"
+        subTitulo="Angular 11"
+        descricao="Construindo uma aplicação com a plataforma Google"
+        img={imagemLivro}
+      />
+
+      <CardRecomenda 
+        titulo="Talvez você se interesse na Alura"
+        subTitulo="Angular 12"
+        descricao="Construindo uma aplicação com a plataforma Google"
+        img={imagemLivro}
+      />
     </UltimosLancamentoContainer>
   )
 }
